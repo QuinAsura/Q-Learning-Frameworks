531fa528aae16f8d1d30e2d37f331f76840a286f
Sibi Sankar <sibi.vasank@gmail.com>
2016-12-12 01:01:14 +0530

    comments added

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 
-#Reinforced_learning_Frameworks
+#Q-Learning-Framework
 This is a general framework that could be tweaked to run your own Q-learning RL algorithms. 
 
 ##Required
-It is implemented in python 2.7. It contains an example which will put you on track for twisting the framework to suit your purpose
+It is implemented in python 2.7. It contains an example which will put you on track for twisting the framework to suit your purpose.
 
 ####[Numpy](http://www.numpy.org/)
 
 ####[Pylab](http://wiki.scipy.org/PyLab)
 
 ##Conference Publications 
-The example used to teach Q-learning RL algorithm is taking up the task to teaching a bot when to charge or discharge a battery. It also houses a neural network trainer and predictor for solar power output.
+The example used to teach Q-learning RL algorithm is taking up the task to teaching an agent when to charge or discharge a battery. It also houses a neural network trainer and predictor for solar power output in matlab.
 
 `Leo, R.; Milton, R.S.; Sibi, S., ”Reinforcement learning for optimal energy management of a solar microgrid,”
 Global Humanitarian Technology Conference - South Asia Satellite (GHTC-SAS), 2014 IEEE
--- q_learning_with_discount_factor.py
+++ q_learning_with_discount_factor.py
@@ -1,33 +1,81 @@
 #!/usr/bin/python
-import numpy as np,itertools,pylab,random,sys
-'''Global variables declaration
-2 Actions sequences
-96 possible scenarios based on Pwt,Dt,Batterylevel of the form 1|4|1 with combination value of 4*4*6'''
-"Prediction belief"
-discount_factor=0.8
-"learning/exploring ratio exploring 50% of the time and learning 50% of the time"
-exploration_ratio=0.6
-"Battery Parameters"
-Bmax=6000
-Blevel=0
-'data for plotting'
+#
+#    license-generator
+#    Copyright (C) 2015-16  Sibi Sankar <http://sibisankar.me>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#
+
+# Libraries Imports
+
+import numpy as np,pylab
+import itertools
+import random,sys
+
+
+# Global variables declaration
+# Actions sequences Charge | Discharge
+# Discretising Solar Power, Battery Level and Discharge rate into states (Larger the number of states larger the lookup table)
+# PWT -- 4 states
+# Battery Level -- 6 states
+# Discharge Rate -- 4 states
+# 96 possible scenarios based on Pwt,Dt,Batterylevel of the form eg 1|3|2, 1|1|6, 2|2|4 with combination value of 4*4*6
+
+
+# HyperParameters
+# Prediction belief
+discount_factor = 0.8
+
+#Learning/exploring ratio
+exploration_ratio = 0.6
+
+# Battery Parameters
+# Battery is modeled as follows:
+# It can charge a maximum of 1000 units per unit time
+# It can charge a maximum of 1000 units per unit time
+# It can only charge or discharge per unit time
+
+
+Bmax = 6000
+Blevel = 0
+
+# Data initialised for plotting
 histogram_data=[]
 Solar_power_utilised=[]
 Load_delivered=[]
-'State Mapping'
-def map_action(code):
-	map_action={0:'1',1:'02',2:'13',3:'24',4:'35',5:'4'}
-	return map_action[code]
+
+
+
+# QvalTable Object
+# Function approx using Lookup Table
+
+
 class QvalTable(object):
-	def __init__(self,alpha,gamma):
+
+    def __init__(self,alpha,gamma):
 		self.Q_val_table=np.zeros((2,96))
 		'Tuning Learning Rate'
 		self.alpha=0.6
 		'Tuning Discount Rate'
 		self.gamma=0.4
-	def get_max_index(self,col):
+
+    def get_max_index(self,col):
 		return np.argmax(self.Q_val_table[:,col])
-	def update_qtable(self,reward,row,col,u_b_level):
+
+    def update_qtable(self,reward,row,col,u_b_level):
 		next_max=self.get_next_max_val(u_b_level)
 		self.Q_val_table[row,col]= self.Q_val_table[row,col] +self.alpha*(reward + self.gamma*(next_max)- self.Q_val_table[row,col])
 
@@ -35,14 +83,22 @@ class QvalTable(object):
 		temp=[]
 		code=Sortb_level(level,6000)
 		states=map_action(int(code))
-		#states+=code
 		for i in scenario_dict:
 			if i[-1] in states:
 				temp.append(max(self.Q_val_table[:,scenario_dict[i]]))
 		return max(temp)
+        
 	def __str__(self):
-		'''Needs to be improved'''
 		print self.Q_val_table
+
+# State Mapping
+
+def map_action(code):
+    map_action={0:'1',1:'02',2:'13',3:'24',4:'35',5:'4'}
+        return map_action[code]
+
+# Load Data
+
 def Load_Data(filedir):
 	temp=[]
 	tempfile=open(filedir,'r')
@@ -50,6 +106,10 @@ def Load_Data(filedir):
 	for i in range(len(chunk)):
 		temp.append(float(chunk[i]))
 	return temp
+
+
+# Scenario Creation
+
 def scenario_creation(plevels,blevels):
 	dic={}
 	p_level_list=[str(i) for i in range(plevels)]
@@ -59,6 +119,9 @@ def scenario_creation(plevels,blevels):
 	for i in range(len(temp)):
 		dic[temp[i].replace("|","")]=i
 	return dic
+
+# Mapping PD: Value to Index'
+
 def Sortpd_level(level,levelmax):
 	temp=''
 	if 0<=level<=(levelmax/4.):
@@ -70,6 +133,9 @@ def Sortpd_level(level,levelmax):
 	else:
 		temp='3'
 	return temp
+
+# Mapping Battery Level: Value to Index
+
 def Sortb_level(level,levelmax):
 	temp=''
 	if 0.<=level<=(levelmax/6.):
@@ -86,11 +152,15 @@ def Sortb_level(level,levelmax):
 
 		temp='5'
 	return temp
+
+
 def Getc_Code(pwt,dwt,blevel):
 	tempP=Sortpd_level(pwt,7000)
 	tempD=Sortpd_level(dwt,4000)
 	tempB=Sortb_level(blevel,6000)
 	return tempP+tempD+tempB
+
+
 def Getr_Code(lrat,code):
 	if random.random() > lrat:
 		return action_dict.keys()[action_dict.values().index(random.randint(0,1))]
@@ -98,21 +168,28 @@ def Getr_Code(lrat,code):
 		colnum=scenario_dict[code]
 		temp=Qvalobj.get_max_index(colnum)
 		return action_dict.keys()[action_dict.values().index(temp)]
+
 def Plot_Hist(data):
 	pylab.hist(data, bins=96)
 	pylab.show()
-''''Loading data from file
-Setting seed for reproduceablity of results
-Normal distribution with mean 600 and standard deviation of size 8760
-1 - charging 0- discharging
-Dictionary containing the number of scenarios
-Dictionary conataining action
-Creation of the QvalTableObject'''
+
+
+
+# Loading data from file
+# Setting seed for reproduceablity of results
+# Normal distribution with mean 600 and standard deviation of size 8760
+# 1 - charging 0- discharging
+# Dictionary containing the number of scenarios
+# Dictionary conataining action
+# Creation of the QvalTableObject
+
+# Uncomment to Test Behavior with constant seed
 #random.seed(1)
 #np.random.seed(1)
+
+# Data Initialisation
 Pwt=Load_Data('PWT.mat')
 Dt =Load_Data('Dtlist.mat')
-#Dt=np.random.normal(loc=3000.0, scale=1000.0, size=8760)
 scenario_dict = scenario_creation(4,6)
 action_dict = {'1':1,'0':0}
 Qvalobj=QvalTable(0.8,0.8)
@@ -120,11 +197,9 @@ solarlist=[]
 loadlist=[]
 powerutil=[]
 qlist=[]
-print 'Learning'
-print 'No. of Training years'
+
 for k in range(100):
 	for j in range(1):
-
 		for i in range(len(Pwt)):
 			Ccode=Getc_Code(Pwt[i],Dt[i],Blevel)
 			Rcode=Getr_Code(0,Ccode)
@@ -141,7 +216,6 @@ for k in range(100):
 				Load_delivered.append(flow)
 			else:
 	 		  Solar_power_utilised.append(flow)
-	print 'fucker'
 	temp=np.copy(Qvalobj.Q_val_table)
 	qlist.append(temp)
 	temp=0
@@ -150,10 +224,13 @@ for k in range(100):
 	powerutil.append(round((sum(Dt))-(sum(Solar_power_utilised)/(j+1)),2))
 	Solar_power_utilised=[]
 	Load_delivered=[]
+
 print 'Learning Period Over with following statistics:'
 print solarlist
 print loadlist
 print powerutil
+
+# Plot Data using Pylab
 #pylab.ylim(0,1.0,'k')
 pylab.plot(range(len(solarlist)),solarlist)
 pylab.show()
@@ -164,11 +241,13 @@ pylab.show()
 pylab.plot(range(len(powerutil)),powerutil,'k')
 pylab.show()
 Plot_Hist(histogram_data)
-'Using Learnt Knowledge Depending only on Q-val table for descision making'
+
+
+# Validation of Learnt Knowledge Depending only on Q-val table for descision making'
 histogram_data=[]
 Load_delivered=[]
 Solar_power_utilised=[]
-#print Qvalobj.Q_val_table
+
 for k in range(1):
 	for j in range(1):
 		for i in range(len(Pwt)):
@@ -203,25 +282,10 @@ pylab.plot(range(len(powerutil)),powerutil,'k')
 pylab.show()
 Plot_Hist(histogram_data)
 
-#Required Graphs
-#Probability of occurence of states -1
-#for different levels of alpha the saturation level in steps
-#redefing data ---done
-#8-10,2kw
-#10-1-,4kw
-#1-2,1kw
-#2-4,4kw
-#server 1kw
-#research 2kw
-#already existing 3 graphs grid should go down   --done
-#nrel.gov radiation get length and breadth of the panel ---done
-#show without learning and with learning.---done(scope for some improvement comparing with another algorithm instead of random actions)
-#choose the best possible values of to get smooth power
-#temperature average as usual---done
+
 print len(qlist)
 
 for i in range(len(qlist)):
-	#print 'cock sucker'
 	#np.savetxt('test.txt', qlist[i])
 	#print np.sum(np.sum((qlist[0]-qlist[1])))/(2*96.0)
 
